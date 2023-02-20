7fc04cd4f71e6d44d651fdc13f8ac7d83c094d21
HP <HP@DESKTOP-VP5V6ES>
2023-02-20 10:33:55 +0200

    Add project files.

---
+++ Contact Us/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Contact_Us.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contact_Us.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
---
+++ Contact Us/Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contact_Us.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter the name")]
+        [StringLength(maximumLength: 10, MinimumLength = 1, ErrorMessage = "Length must be between 1 to 10")]
+        public string Name { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter the email")]
+        [StringLength(maximumLength: 25, MinimumLength = 10, ErrorMessage = "Length must be between 10 to 25")]
+        public string Email { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter the massage")]
+        [StringLength(maximumLength: 25, MinimumLength = 10, ErrorMessage = "Length must be between 10 to 25")]
+        public string Message { get; set; }
+    }
+}
